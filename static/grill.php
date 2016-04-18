6b95573f2a88e73f0f5c5001efb5cd237de50d2c
Farfie <forumsandwebmastersu@email.com>
2016-04-18 16:44:05 -0400

    Update grill.php

--- static/grill.php
+++ static/grill.php
@@ -1,16 +1,13 @@
 <?php
-$waifus = 10;
+$waifus = 1;
 $aWaifus = 0;
 $images = array();
-
 for ($i = 1; $i <= $waifus; $i++) {
 	$images[] = '/img/'.$i.'.png';
 }
-
 for ($i = 1; $i <= $aWaifus; $i++) {
 	$images[] = '/img/'.$i.'.gif';
 }
-
 if (headers_sent() === false) {
 	header('Location: ' . $images[mt_rand(1, count($images))], true, 303);
 }
